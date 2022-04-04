a4916390803411b8bfbb7abcd1fa9daee4e3dec1
DanielWestiner <daniel.westiner@gmail.com>
2022-04-04 13:44:19 -0400

    initial file structure and setup

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const park = require('./models/park');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+app.get('/parks', async (req, res) => {
+    const parks = await park.find({});
+    res.render('parks/index', { parks })
+});
+app.get('/parks/new', (req, res) => {
+    res.render('parks/new');
+})
+
+app.post('/parks', async (req, res) => {
+    const park = new park(req.body.park);
+    await park.save();
+    res.redirect(`/parks/${park._id}`)
+})
+
+app.get('/parks/:id', async (req, res,) => {
+    const park = await park.findById(req.params.id)
+    res.render('parks/show', { park });
+});
+
+app.get('/parks/:id/edit', async (req, res) => {
+    const park = await park.findById(req.params.id)
+    res.render('parks/edit', { park });
+})
+
+app.put('/parks/:id', async (req, res) => {
+    const { id } = req.params;
+    const park = await park.findByIdAndUpdate(id, { ...req.body.park });
+    res.redirect(`/parks/${park._id}`)
+});
+
+app.delete('/parks/:id', async (req, res) => {
+    const { id } = req.params;
+    await park.findByIdAndDelete(id);
+    res.redirect('/parks');
+})
+
+
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
